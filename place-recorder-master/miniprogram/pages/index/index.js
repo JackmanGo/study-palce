c31de83b01f2449203af1132dc07e947f6898873
wangxi <wangxi@eccom.com.cn>
2025-09-07 00:15:47 +0800

    commit

--- place-recorder-master/miniprogram/pages/index/index.js
+++ place-recorder-master/miniprogram/pages/index/index.js
@@ -29,7 +29,13 @@ var page = {
     location_details: undefined
 
   },
-
+  onChooseAvatar:function(e) {
+    const { avatarUrl } = e.detail 
+    console.log(e.detail )
+    this.setData({
+      avatarUrl,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -41,7 +47,7 @@ var page = {
       });
       // console.log("1");
     } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 由于 getUserProfile 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
@@ -51,8 +57,8 @@ var page = {
         // console.log("2")
       };
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
+      // 在没有 open-type=getUserProfile 版本的兼容处理
+      wx.getUserProfile({
         success: res => {
           app.globalData.userInfo = res.userInfo;
           this.setData({
@@ -120,31 +126,43 @@ var page = {
     });
   },
 
-  getUserInfo: function(e) {
-    console.log(e);
-    if (e.detail.userInfo) {
-      app.globalData.userInfo = e.detail.userInfo;
-      console.log("初次授权 e.detail.userInfo: ", e.detail.userInfo);
-      this.setData({
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true
-      });
-
-      this.uploadUserInfo();
-
-      // 冗余调用一次 wx.getUserInfo()，确保云开发控制台—>运营分析->用户访问列表中出现用户详细信息。
-      wx.getUserInfo({
-        success: res => {
-          console.log("初次授权 wx.getUserInfo: ", res.userInfo)
+  getUserProfile: function(res) {
+    wx.getUserProfile({
+      desc: '确认打卡人身份',
+      success: (e) => {
+        console.log("获取用户信息成功", e)
+        if (e.userInfo) {
+          app.globalData.userInfo = e.userInfo;
+          console.log("初次授权 e.detail.userInfo: ", e.userInfo);
+          this.setData({
+            userInfo: e.userInfo,
+            hasUserInfo: true
+          });
+    
+          this.uploadUserInfo();
+    
+          // 冗余调用一次 wx.getUserProfile()，确保云开发控制台—>运营分析->用户访问列表中出现用户详细信息。
+          wx.getUserProfile({
+            success: res => {
+              console.log("初次授权 wx.getUserProfile: ", res.userInfo)
+            }
+          });
+    
+        } else {
+          wx.showToast({
+            icon: "none",
+            title: '登录失败，请重试'
+          });
         }
-      });
-
-    } else {
-      wx.showToast({
-        icon: "none",
-        title: '登录失败，请重试'
-      });
-    }
+      },
+      fail: res => {
+        wx.showToast({
+          icon: "none",
+          title: '登录失败，请重试；'+res
+        });
+      }
+})
+    
   },
 
   uploadUserInfo: function() {
